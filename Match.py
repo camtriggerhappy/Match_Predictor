90db29644dc8bb2737c5164448552464cec44661
Cameron <ckiw23@gmail.com>
2020-03-11 20:22:23 -0400

    Added stuff to Match

--- Match.py
+++ Match.py
@@ -1,5 +1,38 @@
+import FRCTeam
+from enum import Enum
+
+
+class Victor(Enum):
+    BlueWins = 1
+    RedWins = 2
+    Tie = 3
 class Match:
     #Teams should be an array
-    def __init__(self, MatchNum,Teams):
+    def __init__(self, MatchNum,BlueTeams,RedTeams):
         self.MatchNum = MatchNum
-        self.Teams = Teams
+        self.BlueTeams = BlueTeams
+        self.RedTeams = RedTeams
+
+
+    
+
+
+    def Update(self,NewTeam,ReplacedTeam):
+        for i in Teams:
+            if Teams[i] == ReplacedTeam:
+                Teams[i] = NewTeam
+
+
+    def Result(self):
+        BlueScore = 0
+        RedScore = 0
+        for Playing in BlueTeams:
+            BlueScore += BlueTeams[Playing].FindAverage()
+        for Playing in RedTeams:
+            RedScore += RedTeams[Playing].FindAverage()
+        if BlueScore > RedScore:
+            return Victor.BlueWins
+        if BlueScore < RedScore:
+            return Victor.RedWins
+        if BlueScore == RedScore:
+            return Victor.Tie
